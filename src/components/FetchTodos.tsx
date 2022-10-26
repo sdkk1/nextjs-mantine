155af53bca273e3af650e6f1647c981994951365
Keita <fast.break.baske03@gmail.com>
2022-10-26 22:56:58 +0900

    add: FetchTodos コンポーネントを作成

---
+++ src/components/FetchTodos.tsx
@@ -0,0 +1,50 @@
+import { ExclamationIcon } from '@heroicons/react/solid'
+import { Alert, Center, Loader, Text } from '@mantine/core'
+
+import useQueryTodos from '@/hooks/useQueryTodos'
+
+const FetchTodos = () => {
+  const { data, status } = useQueryTodos()
+
+  if (status === 'loading') {
+    return (
+      <Center>
+        <Loader
+          color='indigo'
+          size='xl'
+          variant='bars'
+        />
+      </Center>
+    )
+  }
+
+  if (status === 'error') {
+    return (
+      <Alert
+        icon={<ExclamationIcon />}
+        title='Fetch Error!'
+        color='red'
+        radius='md'
+      >
+        Something wrong happend !
+      </Alert>
+    )
+  }
+
+  return (
+    <div className='text-center'>
+      <Text weight='bold'>Task List</Text>
+      {data?.map((todo) => (
+        <Text
+          my='xs'
+          size='sm'
+          key={todo.id}
+        >
+          {todo.title}
+        </Text>
+      ))}
+    </div>
+  )
+}
+
+export default FetchTodos
