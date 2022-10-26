c4335f3c0f12dabc553ab5b1b5ed8bde7ada9801
Keita <fast.break.baske03@gmail.com>
2022-10-26 23:01:41 +0900

    update: session に応じてページを切り替える処理を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react'
+
+import Auth from '@/components/Auth'
+import DashBoard from '@/components/DashBoard'
+import useStore from '@/store'
+import { supabase } from '@/utils/supabase'
+
 import type { NextPage } from 'next'
 
-const Home: NextPage = () => (
-  <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-)
+const Home: NextPage = () => {
+  const session = useStore((state) => state.session)
+  const setSession = useStore((state) => state.setSession)
+  useEffect(() => {
+    setSession(supabase.auth.session())
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [setSession])
+  return <>{session ? <DashBoard /> : <Auth />}</>
+}
 
 export default Home
